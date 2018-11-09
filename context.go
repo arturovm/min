f0b3d85a774b9e8709b68218ceec531aa7cffd56
Arturo Vergara <hello@arturovm.me>
2018-11-09 13:52:07 -0600

    Parameter handling is now cleaner and faster

--- context.go
+++ context.go
@@ -7,26 +7,26 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-// contextKey is an alias of string, for use with request contexts
-type contextKey string
+// key is an alias of int, for use with request contexts
+type key int
+
+// paramsKey is the key that identifies httprouter.Params in a request's
+// context
+var paramsKey key
 
 func wrapHandler(f http.Handler) httprouter.Handle {
 	return httprouter.Handle(func(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 		ctx := r.Context()
-		for i := range params {
-			ctx = context.WithValue(ctx, contextKey(params[i].Key), params[i].Value)
-		}
+		ctx = context.WithValue(ctx, paramsKey, params)
 		f.ServeHTTP(w, r.WithContext(ctx))
 	})
 }
 
 // GetParam gets a parameter from a given request
 func GetParam(r *http.Request, key string) string {
-	if ctx := r.Context(); ctx != nil {
-		if val := ctx.Value(contextKey(key)); val != nil {
-			if s, ok := val.(string); ok {
-				return s
-			}
+	if val := r.Context().Value(paramsKey); val != nil {
+		if p, ok := val.(httprouter.Params); ok {
+			return p.ByName(key)
 		}
 	}
 	return ""
